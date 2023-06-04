de305fe24740dab8f88bfd64b30a8f9b0ee339df
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-06-04 15:05:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+
+const currencies = new Map (
+    ['USD', 'United States Dollar'],
+    ['EUR', 'Euro'],
+    ["CNY", 'Chinese yuan']
+);
+
+const transactions = [300, 250, 5000, 1500, -1000, 200];
+
+const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'y', 'q'];
+//letters.slice(1, 5);
+console.log(letters.slice(3));
+
+const slicedLetters = letters.slice(-3);
+console.log(slicedLetters);
